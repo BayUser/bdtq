a2a17e93a28dab4ad201d4d5f19f9aa7660188b7
Glitch (indigo-fantasy-sword) <none>
2023-11-13 10:32:44 +0000

    🕹🎭 Checkpoint ./config.js:62047072/1151 ./package.json:62047072/55

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 module.exports = {
-    clientSecret: "aOEEZ0vdPwAXKIBRdFHfC6o9SstShagd",
-    clientID: "1165574590966267954",
-    token: "",
-    mongo: "",
+    clientSecret: process.env.secret,
+    clientID: "1072592067043278989",
+    token: process.env.token,
+    mongo: process.env.mongo,
     prefix: ".",
-    redirectURI: "ht.13:7056",
-    port: 7056,
-    owners: ["1143898111291441152", "1145452198873006212"],
-    redirectionIP: ["h"],
-    botlink: ["https://discord.com/oauth2/authorize?client_id=1165574590966267954&scope=bot&permissions=8"],
-    redirectionBot: ["http:056"],
-    authlink: "https://discord.com/api/oauth2/authorize?client_id=1165574590966267954&redirect_uri=http%3A%2F%2F45.136.4.57%3A7056&response_type=code&scope=identify%20guilds.join",
-    webhookURL: "https://discord.com/api/webhooks/1165579672294260796/sDk_OGO1FBepd-OcvuXfjRbZXDQUvfIzIdCay6UjvMkG1NP3b5-rzLnUP0Ek4_dCvzZ1",
-
-
-   webhookURL2: "https://discord.com/api/webhooks/1165579672294260796/sDk_OGO1FBepd-OcvuXfjRbZXDQUvfIzIdCay6UjvMkG1NP3b5-rzLnUP0Ek4_dCvzZ1"
+    redirectURI: "http://172.17.0.81:3030",
+    port: 3030,
+    owners: ["1143898111291441152"],
+    redirectionIP: ["172.17.0.81:3030"],
+    botlink: ["https://discord.com/oauth2/authorize?client_id=1072592067043278989&scope=bot&permissions=8"],
+    redirectionBot: ["http://172.17.0.81:3030"],
+    authlink: "https://discord.com/api/oauth2/authorize?client_id=1072592067043278989&redirect_uri=http%3A%2F%2F172.17.0.81%3A3030&response_type=code&scope=identify%20guilds.join",
+    webhookURL: "https://discord.com/api/webhooks/1172975707446595674/EuJjYnx0RjVXB8y58XyGMikDpCLr7YjQWkZNenclvS5j4axHcomyvvHl7Mv2NLvHLrQi",
+    webhookURL2: "https://discord.com/api/webhooks/1172975707446595674/EuJjYnx0RjVXB8y58XyGMikDpCLr7YjQWkZNenclvS5j4axHcomyvvHl7Mv2NLvHLrQi"
 }
